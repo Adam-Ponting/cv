0f25c963a68892b58ab957f5a43340b5a8043462
Adam <adam.ponting@gmail.com>
2019-07-28 16:23:58 +0100

    updated welcome

--- src/router.js
+++ src/router.js
@@ -39,15 +39,6 @@ export default new Router({
       name: 'product',
       component: ProductLandingPage
     },
-    {
-      path: '/tennis',
-      name: 'tennis',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ './components/tennis/TennisHome.vue')
-    },
     /*
     https://router.vuejs.org/guide/essentials/history-mode.html#example-server-configurations
     */
