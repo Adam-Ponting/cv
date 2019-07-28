532fcd19e39186d06842e12d6ac1137d1c5208d2
Adam <adam.ponting@gmail.com>
2019-07-28 13:25:07 +0100

    updated splash image and navbar

---
+++ src/components/AppBackgroundImage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="splash-image">
+    <span class="splash-image__content">
+      <slot name="splashContent"></slot>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/css/colors.scss'; // imports variable to this component
+
+// splash image
+.splash-image {
+  margin-top: 64px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: url('https://firebasestorage.googleapis.com/v0/b/adp-cv.appspot.com/o/home%2Fsunset.jpg?alt=media&token=999208b6-d3c1-40a5-939b-5ba9478581fb')
+    no-repeat center;
+  background-size: cover;
+  height: 50vh;
+}
+.splash-image__content {
+  text-transform: capitalize;
+  font-variant: small-caps;
+  font-size: 5em;
+  letter-spacing: 2px;
+  font-weight: bold;
+  padding: 0 1rem;
+  color: $font-light;
+  cursor: default;
+}
+@media screen and (max-width: 800px) {
+  .splash-image {
+    height: 30vh;
+  }
+  .splash-image__content {
+    font-size: 2em;
+  }
+}
+</style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -97,7 +97,7 @@ export default {
 
 .nav {
   display: flex;
-  background: $lighter;
+  background: $bg-light;
   position: fixed; // move to top
   top: 0;
   width: 100%; // fill width
@@ -120,7 +120,7 @@ export default {
 .nav__link {
   font-weight: bold;
   padding: 0.5rem;
-  color: $pink;
+  color: $interact;
   display: flex;
   justify-content: center;
 }
@@ -133,7 +133,7 @@ export default {
   border-top: 3px solid transparent;
   transition: border-bottom 0.5s;
   &:hover {
-    border-bottom: 3px solid $pink;
+    border-bottom: 3px solid $interact;
   }
 }
 .nav__mobile {
@@ -157,7 +157,7 @@ export default {
     align-items: center;
     font-weight: bold;
     padding: 0.5rem;
-    color: $pink;
+    color: $interact;
   }
   .font-awesome-icon {
     margin-left: 10px;
@@ -171,12 +171,12 @@ export default {
   .mobile-menu-enter {
     transform: translateX(-100px);
     opacity: 0;
-    background: $pink;
+    background: $interact;
   }
   .mobile-menu-leave-to {
     transform: translateX(100px);
     opacity: 0;
-    background: $pink;
+    background: $interact;
   }
   .nav__mobile-links {
     display: flex;
