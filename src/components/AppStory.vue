cb6ae7a196ffdbea0bbf9477f75209abc997b3d1
Adam Ponting <adam.ponting@gmail.com>
2020-04-29 17:48:10 +0100

    story text tweak

--- src/components/AppStory.vue
+++ src/components/AppStory.vue
@@ -14,7 +14,7 @@
               <strong>Adam</strong> and this site is a portfolio of some work I
               have done. I have had an interest in web development since the
               days of dial-up modems and have taught myself the skills I need to
-              allow me to start in this industry. <br />I understand the client
+              allow me to work in this industry. <br />I understand the client
               <strong>experience</strong> is central to business needs and I
               enjoy working in a team and interacting with people of different
               capabilities.
